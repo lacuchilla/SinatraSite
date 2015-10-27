67a5718033c3bcf8d1ca1d65ef95db86b573ccc6
Amy Hunter <amyrhunter@gmail.com>
2015-10-27 14:41:46 -0700

    Added working links to all 4 pages on my site.

--- my-site.rb
+++ my-site.rb
@@ -2,7 +2,19 @@ require "sinatra"
 
 class MySite < Sinatra::Base
 
-  get "/" do
+  get "/index" do
+    erb :index
+  end
 
+  get "/projectsummary" do
+    erb :projectsummary
+  end
+
+  get "/aboutme" do
+    erb :aboutme
+  end
+
+  get "/blogpost" do
+    erb :blogpost
   end
 end
