457d82683ee9b55f1306db54657f6398f91ccf6a
Amy Hunter <amyrhunter@gmail.com>
2015-10-30 10:39:27 -0700

    more tweaking of code

--- my-site.rb
+++ my-site.rb
@@ -3,28 +3,33 @@ require "sinatra"
 class MySite < Sinatra::Base
 
   get "/" do
+    @title = "Amy Hunter"
     erb :index
   end
 
   get "/index" do
+    @title = "Amy Hunter"
     erb :index
   end
 
   get "/project_summary" do
+    @title = "Projects I've Done"
     erb :project_summary
   end
 
   get "/about_me" do
-    # @header_links = ["about_me", "blog_post", "index", "french_poetry", "project_summary"]
+    @title = "About Me"
     erb :about_me
   end
 
   get "/blog_post" do
+    @title = "My Blog"
     @things_i_like = ["Mariners Baseball", "Carving Pumpkins", "Black Cats", "NES Gaming", "Swing Dancing"]
     erb :blog_post
   end
 
   get "/french_poetry" do
+    @title = "French Poetry"
     erb :french_poetry
   end
 end
